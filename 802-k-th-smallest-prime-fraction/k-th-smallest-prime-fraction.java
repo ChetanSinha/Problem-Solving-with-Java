30b9e3321a14eeebbdf0adcd0a9bbafd0bba087a
Chetan Sinha <50909804+ChetanSinha@users.noreply.github.com>
2024-05-11 00:03:13 +0530

    Time: 401 ms (48.03%) | Memory: 69.2 MB (44.98%) - LeetSync

--- 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.java
+++ 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.java
@@ -1,26 +1,17 @@
 class Solution {
-    class Triplets {
-        double fraction;
-        int i;
-        int j;
-    }
     public int[] kthSmallestPrimeFraction(int[] arr, int k) {
-        PriorityQueue<Triplets> pq = new PriorityQueue<>((a, b) -> Double.compare(b.fraction, a.fraction));
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Double.compare((double) b[0]/b[1], (double) a[0]/a[1]));
 
         for (int i=0; i<arr.length; i++) {
             for (int j=i+1; j<arr.length; j++) {
-                Triplets t = new Triplets();
-                t.fraction = (double )arr[i]/arr[j];
-                t.i = arr[i];
-                t.j = arr[j];
-                pq.add(t);
+                int[] pairs = new int[]{arr[i], arr[j]};
+                pq.add(pairs);
                 if (pq.size() > k) {
                     pq.poll();
                 }
             }
         }
 
-        Triplets t = pq.poll();
-        return new int[]{t.i, t.j};
+        return pq.poll();
     }
 }
